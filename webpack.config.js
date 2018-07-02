fbbfb820906755a20863b2582d711256ba38c2fe
Mickie Prasetya <mickie.prasetya@cbn.co.id>
2018-07-02 18:33:28 +0700

    completed header implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
-module.exports =
-{
-   entry: '',
+var path = require('path');
+
+module.exports = {
+   mode: 'none',
+   entry:{
+      App:  './app/assets/scripts/App.js'  
+   },
    output: {
-      path: '',
-      filename: ''
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'app/temp/scripts')
+   },
+   module: {
+      rules: [
+         {
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['env']
+               }
+            },
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/
+         }
+      ]
    }
 }
